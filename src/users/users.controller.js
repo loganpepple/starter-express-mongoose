bc01d898b8d6e87a120617decf8a9e5809c03817
loganpepple <l.pepple.96@gmail.com>
2023-04-21 00:50:33 -0400

    Mongoose part 2 complete

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,17 +1,87 @@
-const User = require("./users.model")
-
+const User = require("./users.model");
 
 async function list(req, res) {
-
   const users = await User.find();
-
   res.send(users);
-
 }
 
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Must include a ${propertyName}`,
+    });
+  };
+}
+
+async function create(req, res) {
+  const { data: { username, email } = {} } = req.body;
+  const newUser = new User({
+    username: username,
+    email: email,
+  });
+  await newUser.save();
+  res.status(201).json({ data: newUser });
+}
+
+async function userExists(req, res, next) {
+  const { userID } = req.params;
+  const foundUser = await User.findOne({ _id: userId });
+  if (foundUser) {
+    res.locals.user = foundUser;
+    return next();
+  }
+  next({
+    status: 404,
+    message: "User id not found: ${userId}",
+  });
+}
+
+function read(req, res, next) {
+  res.json({ data: res.locals.user });
+}
+
+async function update(re, res) {
+  const user = res.locals.users;
+  const { data: { username, email } = {} } = req.body;
+
+  user.username = username;
+  user.email = email;
+  await user.save()
+
+  res.json({ data: user });
+}
+
+async function destroy(req, res) {
+  const { userId } = req.params;
+  await User.deleteOne({ _id: userId });
+  res.sendStatus(204);
+}
 
 module.exports = {
-
   list,
-
+  create: [
+    bodyDataHas("username"),
+    bodyDataHas("email"),
+    create
+  ],
+  update: [
+    userExists,
+    bodyDataHas("username"),
+    bodyDataHas("email"),
+    update
+  ],
+  read: [
+    userExists,
+    read
+  ],
+  delete: [
+    userExists,
+    destroy
+  ],
+  userExists
 };
